faa8d5320573eeb58186b87dbca6875077d86fb2
saad zaheer <salaam7860@gmail.com>
2025-09-24 16:54:23 +0500

    terraform basics

--- Terraform_basics/basics01.tf
+++ Terraform_basics/basics01.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  #resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
